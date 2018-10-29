2fe7c4581a4d72550a3f83c0848fc5ec7b5e811e
PabloRT98 <pabriptor@alum.us.es>
2018-10-29 21:53:40 +0100

    Clases de dominio

---
+++ Clases Modelo de Dominio Java/FixUpTask.java
@@ -0,0 +1,106 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class FixUpTask extends DomainEntity {
+
+	private String	ticker;
+	private Date	moment;
+	private String	description;
+	private String	address;
+	private double	maxPrice;
+	private Date	startTime;
+	private Date	endTime;
+
+
+	public FixUpTask() {
+		super();
+	}
+
+	@NotBlank
+	@Column(unique = true)
+	//	Preguntar cómo utilizar la expresión regular aquí
+	public String getTicker() {
+		return this.ticker;
+	}
+
+	public void setTicker(final String ticker) {
+		this.ticker = ticker;
+	}
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+	@NotBlank
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+	@NotBlank
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(final String address) {
+		this.address = address;
+	}
+	@Min(0)
+	@Digits(integer = 4, fraction = 2)
+	public double getMaxPrice() {
+		return this.maxPrice;
+	}
+
+	public void setMaxPrice(final double maxPrice) {
+		this.maxPrice = maxPrice;
+	}
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getStartTime() {
+		return this.startTime;
+	}
+
+	public void setStartTime(final Date startTime) {
+		this.startTime = startTime;
+	}
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getEndTime() {
+		return this.endTime;
+	}
+
+	public void setEndTime(final Date endTime) {
+		this.endTime = endTime;
+	}
+
+}
