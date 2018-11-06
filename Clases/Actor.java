7a6ad58ebac58e92faf0975ecfa4afe50f9e3ba3
Aguza5 <43997459+Aguza5@users.noreply.github.com>
2018-11-06 18:04:49 +0100

    clases Domain

---
+++ Clases/Actor.java
@@ -0,0 +1,156 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.URL;
+
+import security.UserAccount;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Actor extends DomainEntity {
+
+	private String	name;
+	private String	middleName;
+	private String	surname;
+	private String	photo;
+	private String	email;
+	private String	phoneNumber;
+	private String	address;
+	private int		numSocialProfile;
+	private Message	messageSent;
+	private Message	messageReceived;
+
+
+	public Message getMessageSent() {
+		return this.messageSent;
+	}
+
+	public void setMessageSent(final Message messageSent) {
+		this.messageSent = messageSent;
+	}
+
+	public Message getMessageReceived() {
+		return this.messageReceived;
+	}
+
+	public void setMessageReceived(final Message messageReceived) {
+		this.messageReceived = messageReceived;
+	}
+
+	public Actor() {
+		super();
+	}
+
+	@NotBlank
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+	@NotBlank
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public void setMiddleName(final String middleName) {
+		this.middleName = middleName;
+	}
+	@NotBlank
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(final String surname) {
+		this.surname = surname;
+	}
+	@URL
+	public String getPhoto() {
+		return this.photo;
+	}
+
+	public void setPhoto(final String photo) {
+		this.photo = photo;
+	}
+	@Email
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+	@NotBlank
+	@Pattern(regexp = "^(\\+)([1-9][0-9]{0,2})(\\s)(\\([1-9][0-9]{0,2}\\))(\\s)([0-9]{4,})$")
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	@NotBlank
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(final String address) {
+		this.address = address;
+	}
+	@NotBlank
+	public int getNumSocialProfile() {
+		return this.numSocialProfile;
+	}
+
+	public void setNumSocialProfile(final int numSocialProfile) {
+		this.numSocialProfile = numSocialProfile;
+	}
+
+
+	//relationships
+
+	private Collection<Box>	boxes;
+
+
+	@OneToMany
+	@NotEmpty
+	public Collection<Box> getBoxes() {
+		return this.boxes;
+	}
+
+	public void setBox(final Collection<Box> boxes) {
+		this.boxes = boxes;
+	}
+
+
+	private UserAccount	userAccount;
+
+
+	@NotNull
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	public UserAccount getUserAccount() {
+		return this.userAccount;
+	}
+
+	public void setUserAccount(final UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
+}
